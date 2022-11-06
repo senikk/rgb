24ec78bf8eaee7d9cbdf3ccc861452e0b399aaa7
Terje Pedersen <terjep@unimicro.no>
2022-11-06 07:34:44 +0100

    working example

---
+++ rgbapp/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {
+        Header,
+        HeaderNav,
+        HeaderNavItem,
+        SkipToContent,
+        Content
+    } from "carbon-components-svelte";
+</script>
+
+<Header company="" platformName="VBUKLAN RGB" href="/">
+    <div slot="skip-to-content">
+        <SkipToContent />
+    </div>
+    <HeaderNav>
+        <HeaderNavItem href="/" text="Veksle" />
+        <HeaderNavItem href="/timer" text="Timer" />
+    </HeaderNav>
+</Header>
+
+<Content>
+    <slot></slot>
+</Content>
